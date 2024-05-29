7fbb1103a932360defa91ca868a2c9e2d62b9b54
Anulak-AC <147117464+Anulak-AC@users.noreply.github.com>
2024-05-29 17:30:27 +0700

    Initial commit

---
+++ dcywah/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(546, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(65, 41)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(585, 20)
+        Me.txtPath.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(656, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Load Image"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Image :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "URL :"
+        '
+        'txtURL
+        '
+        Me.txtURL.Location = New System.Drawing.Point(65, 15)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(585, 20)
+        Me.txtURL.TabIndex = 4
+        Me.txtURL.Text = "http://dcywah.dcy.go.th/opp/app/register.php"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(785, 562)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtURL)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "C:\Users\anulak\Downloads\green-hills-panorama-landscape-isolated-on-white-o (1)." &
+    "jpg"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtURL As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ dcywah/Form1.vb
@@ -0,0 +1,225 @@
+﻿
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports System.Threading.Tasks
+Imports OpenQA.Selenium.Support.UI
+Imports System.Net
+Imports Newtonsoft
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Dim urlxx As String = "file:///C:/Users/anulak/Downloads/WYC/%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B8%81%E0%B8%B4%E0%B8%88%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%81%E0%B9%81%E0%B8%A5%E0%B8%B0%E0%B9%80%E0%B8%A2%E0%B8%B2%E0%B8%A7%E0%B8%8A%E0%B8%99.html"
+    ''http://dcywah.dcy.go.th/opp/app/register.php
+    ' Set Chrome options to maximize the window
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Define the URLs to load
+        ' Add more URLs as needed
+
+        ' Create a list to hold tasks for opening pages concurrently
+        Dim tasks As New List(Of Task)()
+        ' Create tasks to open each page and perform actions concurrently
+        For loopX As Integer = 0 To 2
+            Dim randomLocation As String = GenerateLocationInOrder(loopX) ' Generate a random location
+            Dim task As Task = Task.Factory.StartNew(Sub()
+                                                         OpenAndPerformAction(txtURL.Text, randomLocation)
+                                                     End Sub)
+            tasks.Add(task)
+        Next
+        ' Wait for all tasks to complete
+        Task.WaitAll(tasks.ToArray())
+
+
+
+
+    End Sub
+    Function GenerateLocationInOrder(ByVal index As Integer) As String
+        ' Check if the index is within the bounds of the xCoordinates array
+        If index >= 0 AndAlso index < xCoordinates.Length Then
+            ' Get the X-coordinate from the array
+            Dim xCoordinate As Integer = xCoordinates(index)
+
+            ' Define the Y-coordinate (e.g., tail position)
+            Dim yCoordinate As Integer = 450 ' Adjust as needed
+
+            ' Return the formatted location string (X,Y)
+            Return $"{xCoordinate},{yCoordinate}"
+        Else
+            ' Return an empty string or throw an exception, depending on your requirements
+            Return ""
+        End If
+    End Function
+    Private ReadOnly xCoordinates As Integer() = {0, 50, 100, 150, 200, 250, 300, 350, 400, 450}
+
+    ' Function to open a page and perform actions
+    Sub OpenAndPerformAction(ByVal url As String, ByVal position As String)
+        ' Set Chrome options to maximize the window
+        Dim options As New ChromeOptions()
+        options.AddArgument("window-size=450,500")
+        ' Calculate X and Y coordinates based on the loopID
+
+        ' Generate random X and Y coordinates
+        Dim rand As New Random()
+        Dim X As Integer = rand.Next(0, 1000) ' Adjust the range as needed
+        Dim Y As Integer = rand.Next(0, 1000) ' Adjust the range as needed
+
+        options.AddArgument($"window-position={position}")
+        ' Initialize ChromeDriver with options
+        Dim driver As New ChromeDriver(options)
+        ' Navigate to the specified URL
+        driver.Navigate().GoToUrl(url)
+        ' Get the HTTP status of the webpage
+        Dim CH As Integer = 5
+
+        For ICHECK As Integer = 0 To CH
+            ' Check if a button with ID "submitBtn" is present
+            Dim isElementPresent As Boolean = CheckForElement(driver, By.Name("fileUpload"), TimeSpan.FromSeconds(10))
+            CH = CH + 1
+            If isElementPresent Then
+                Exit For
+            Else
+                driver.Navigate().Refresh()
+                Console.WriteLine("Element not found within the specified timeout period.")
+            End If
+            Threading.Thread.Sleep(2500)
+        Next
+        Dim element As IWebElement
+        element = driver.FindElement(By.Name("fileUpload"))
+
+
+
+        Dim filePath As String = txtPath.Text ' "C:\Users\anulak\Downloads\green-hills-panorama-landscape-isolated-on-white-o (1).jpg" ' Update with your file 
+
+        If filePath = "" Then
+            filePath = Application.StartupPath & "S__25649163.jpg"
+        End If
+
+        element.SendKeys(filePath)
+
+        element = driver.FindElement(By.Name("CARD_ID"))
+        element.SendKeys("1369900361691")
+
+        element = driver.FindElement(By.Name("PREFIX_TH"))
+        Dim selectObject As New SelectElement(element)
+        selectObject.SelectByText("นางสาว")
+
+        element = driver.FindElement(By.Name("NAME_TH"))
+        element.SendKeys("อาริษา")
+
+        element = driver.FindElement(By.Name("LASTNAME_TH"))
+        element.SendKeys("วัชรคุปต์")
+
+
+        element = driver.FindElement(By.Name("PREFIX_EN"))
+        selectObject = New SelectElement(element)
+        selectObject.SelectByText("MISS")
+
+        element = driver.FindElement(By.Name("NAME_EN"))
+        element.SendKeys("ARISA")
+
+        element = driver.FindElement(By.Name("LASTNAME_EN"))
+        element.SendKeys("WATCHARAKUP")
+
+        element = driver.FindElement(By.Name("EMAIL"))
+        element.SendKeys("Arisa.watcharakup1996@gmail.com")
+
+
+        element = driver.FindElement(By.Name("ADDRESS"))
+        element.SendKeys("125/199 หมู่7 ตึกโบว์ เปรมฤทัย ซ.4 ต.บางโฉลง อ. บางพลี สมุทรปราการ 10540")
+
+
+        element = driver.FindElement(By.Name("passw2"))
+        Dim OTPX As String = element.GetAttribute("value")
+
+        element = driver.FindElement(By.Name("capt"))
+        element.SendKeys(OTPX)
+
+        element = driver.FindElement(By.Name("ckAccept"))
+        element.Click()
+
+        element = driver.FindElement(By.Name("btLogin"))
+        element.Click()
+
+        'driver.Quit()
+    End Sub
+    Function CheckForElement(ByVal driver As IWebDriver, ByVal locator As By, ByVal timeout As TimeSpan) As Boolean
+        Dim wait As New WebDriverWait(driver, timeout)
+        Try
+            ' Wait until the condition is true (the element is located)
+            wait.Until(Function(d) d.FindElements(locator).Count > 0)
+            Return True ' Return true if the element is found within the timeout period
+        Catch ex As WebDriverTimeoutException
+            ' If timeout occurs, output a message indicating a timeout
+            Return False ' Return false if the element is not found within the timeout period
+        End Try
+    End Function
+    Sub InteractWithElements(driver As IWebDriver)
+        ' Interact with a text field
+        Dim textField As IWebElement = driver.FindElement(By.Id("text_field_id"))
+        textField.SendKeys("Your text here")
+        ' Interact with a select element (dropdown)
+        Dim selectElement As IWebElement = driver.FindElement(By.Id("select_element_id"))
+        Dim selectObject As New OpenQA.Selenium.Support.UI.SelectElement(selectElement)
+        selectObject.SelectByText("Option Text") ' or SelectByValue or SelectByIndex
+
+        ' Interact with a button
+        Dim button As IWebElement = driver.FindElement(By.Id("button_id"))
+        button.Click()
+        ' Additional interactions as needed
+    End Sub
+
+    Function CheckElementExists(ByVal driver As IWebDriver, ByVal locator As By) As Boolean
+        Try
+            ' Attempt to find the element
+            Dim element As IWebElement = driver.FindElement(locator)
+            ' If no exception is thrown, the element exists
+            Return True
+        Catch ex As NoSuchElementException
+            ' If NoSuchElementException is thrown, the element does not exist
+            Return False
+        End Try
+    End Function
+
+
+
+    Function MeasurePageLoadStatus(ByVal driver As IWebDriver, ByVal url As String) As Boolean
+        Dim stopwatch As New Stopwatch()
+        Try
+            ' Start stopwatch to measure time
+            stopwatch.Start()
+            ' Navigate to the webpage
+            driver.Navigate().GoToUrl(url)
+            ' Wait until the page is fully loaded
+            Dim wait As New WebDriverWait(driver, TimeSpan.FromSeconds(30)) ' Set timeout as needed
+            wait.Until(Function(d) CType(d, IJavaScriptExecutor).ExecuteScript("return document.readyState") = "complete")
+            ' Stop the stopwatch
+            stopwatch.Stop()
+
+            ' Return True indicating successful page load
+            Return True
+        Catch ex As WebDriverTimeoutException
+            ' If timeout occurs, output a message indicating a timeout
+            Console.WriteLine("Timeout occurred while loading the page.")
+            ' Return False indicating timeout
+            Return False
+        End Try
+    End Function
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Process.Start("cmd taskkill /f /im chromedriver.exe")
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Show the OpenFileDialog to select an image file
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Image Files (*.jpg, *.jpeg, *.png, *.gif)|*.jpg; *.jpeg; *.png; *.gif"
+        openFileDialog.Multiselect = False ' Allow only single file selection
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Get the selected image file path
+            txtPath.Text = openFileDialog.FileName
+
+        End If
+    End Sub
+End Class
